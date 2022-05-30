7f2fae61e1830fc4e5c7d1b9b07e0ed52f3e8ee9
Lucasrcarvalho <ribeirolucas149@gmail.com>
2022-05-30 20:07:19 -0300

    Service layer, component registration

---
+++ src/main/java/com/lucasrc/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.lucasrc.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasrc.course.entities.User;
+import com.lucasrc.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	//Declare the UserService dependency of DataRepository
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
