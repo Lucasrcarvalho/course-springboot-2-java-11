9fe1d93e4ac4954f0e529af9160eb74cb3d9c506
Lucasrcarvalho <ribeirolucas149@gmail.com>
2022-06-12 21:00:34 -0300

    Payment, one-to-one association

--- src/main/java/com/lucasrc/course/config/TestConfig.java
+++ src/main/java/com/lucasrc/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lucasrc.course.entities.Category;
 import com.lucasrc.course.entities.Order;
 import com.lucasrc.course.entities.OrderItem;
+import com.lucasrc.course.entities.Payment;
 import com.lucasrc.course.entities.Product;
 import com.lucasrc.course.entities.User;
 import com.lucasrc.course.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);//Salvar objeto dependente numa relação um para um
+		
+		orderRepository.save(o1);
 	}
 }
